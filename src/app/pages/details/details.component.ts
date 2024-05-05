9e754fe1e2e096e1e196fa198753044d01dfae5c
Daniel Levi <daniellevi000@gmail.com>
2024-05-05 00:37:37 -0300

    feat: pages details in section comments

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
